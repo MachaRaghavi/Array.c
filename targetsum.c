bf07fbf7ead8eab6ffcb46ceb738d8403a71b0c9
MachaRaghavi <raghavimacha14@gmail.com>
2026-02-25 22:22:17 +0530

    Add files via upload

---
+++ targetsum.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+int size;
+printf("Enter the size of array:\n");
+scanf("%d",&size);
+int a[size];
+for(int i=0;i<=(size-1);i++)
+{
+printf("Enter the %d value:",(i+1));
+scanf("%d",&a[i]);
+}
+int a1,b1;
+int flag=0;
+int k;
+printf("Enter your target value:");
+scanf("%d",&k);
+for(int i=0;i<(size-1);i++)
+{
+for(int j=0;j<=(size-1);j++){
+if(a[i]+a[j]==k)
+{a1=a[i];
+b1=a[j];
+flag++;}
+}
+}
+if(flag!=0){
+printf("a1=%d\t b1=%d",a1,b1);}
+else{
+printf("-1");
+}
+return 0;
+}
